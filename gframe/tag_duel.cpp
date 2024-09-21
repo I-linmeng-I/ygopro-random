90da509e2fc9d661d45eb4cb7caa6453bb3bdf23
I-linmeng-I <48974182+I-linmeng-I@users.noreply.github.com>
2024-09-21 22:31:08 +0800

    Update tag_duel.cpp

--- gframe/tag_duel.cpp
+++ gframe/tag_duel.cpp
@@ -2375,6 +2375,7 @@ void TagDuel::RefreshSingle(int player, int location, int sequence, int flag) {
 	BufferIO::WriteInt8(qbuf, location);
 	BufferIO::WriteInt8(qbuf, sequence);
 	int len = query_card(pduel, player, location, sequence, flag, qbuf, 0);
+	const int clen = BufferIO::ReadInt32(qbuf);
 	auto position = GetPosition(qbuf, 12);
 	if(location & LOCATION_ONFIELD) {
 		int pid = (player == 0) ? 0 : 2;
